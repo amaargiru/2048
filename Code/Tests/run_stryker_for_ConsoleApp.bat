eac028c34f2afb5e04bf194adbeb8363c6f524db
amaargiru <war4one@gmail.com>
2022-02-18 11:21:27 +0500

    Add Stryker mutation testing Создал пару bat-ников для запуска Striker. Читаю отчеты, думаю, как это лучше применить

---
+++ Code/Tests/run_stryker_for_ConsoleApp.bat
@@ -0,0 +1,4 @@
+REM Run Stryker mutation testing for ConsoleApp project
+dotnet stryker -p "C:\Works\2048\Code\ConsoleApp\ConsoleApp.csproj"
+
+pause
---
+++ Code/Tests/run_stryker_for_GameCore.bat
@@ -0,0 +1,4 @@
+REM Run Stryker mutation testing for GameCore project
+dotnet stryker -p "C:\Works\2048\Code\GameCore\GameCore.csproj"
+
+pause
