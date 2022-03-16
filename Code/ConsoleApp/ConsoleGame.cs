c2fc4d3629c5e4994fb82ee1b30f10992ef2ef59
amaargiru <war4one@gmail.com>
2022-03-16 15:48:42 +0500

    Delete Serilog

--- Code/ConsoleApp/ConsoleGame.cs
+++ Code/ConsoleApp/ConsoleGame.cs
@@ -1,95 +1,82 @@
 ﻿using GameCore;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using Serilog;
 using Console = Colorful.Console;
 
 namespace ConsoleApp;
 
 internal static class ConsoleGame
 {
-    private static void Main()
-    {
-        var settingsFile = "appsettings.json";
-        var savedGameFile = "gamestate.json";
+   private static void Main()
+   {
+      var settingsFile = "appsettings.json";
+      var savedGameFile = "gamestate.json";
 
-        GameState gameState;
-        var game = new Game();
-        var gameSettings = new GameSettings();
-        var colorSettings = new ColorSettings();
+      GameState gameState;
+      var game = new Game();
+      var gameSettings = new GameSettings();
+      var colorSettings = new ColorSettings();
 
-        // Logging into file only, console is for game
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.File(@"log\log2048.txt",
-                fileSizeLimitBytes: 1000_000,
-                retainedFileCountLimit: 5,
-                rollOnFileSizeLimit: true)
-            .CreateLogger();
+      Console.CursorVisible = false;
 
-        Console.CursorVisible = false;
+      // Binding settings from json
+      var config = new ConfigurationBuilder().AddJsonFile(settingsFile, optional: true).Build();
+      config.GetSection("GameSettings").Bind(gameSettings);
+      config.GetSection("ColorSettings").Bind(colorSettings);
 
-        // Binding settings from json
-        var config = new ConfigurationBuilder().AddJsonFile(settingsFile, optional: true).Build();
-        config.GetSection("GameSettings").Bind(gameSettings);
-        config.GetSection("ColorSettings").Bind(colorSettings);
-
-        if (File.Exists(savedGameFile))
-        {
-            try
-            {
-                // Binding saved game from json
-                gameState = JsonConvert.DeserializeObject<GameState>(File.ReadAllText(savedGameFile));
-            }
-            catch
-            {
-                // Start default game
-                gameState = game.Init(gameSettings.BoardRows, gameSettings.BoardCols, gameSettings.DigitsOnNewBoard);
-            }
-        }
-        else
-        {
+      if (File.Exists(savedGameFile))
+      {
+         try
+         {
+            // Binding saved game from json
+            gameState = JsonConvert.DeserializeObject<GameState>(File.ReadAllText(savedGameFile));
+         }
+         catch
+         {
             // Start default game
             gameState = game.Init(gameSettings.BoardRows, gameSettings.BoardCols, gameSettings.DigitsOnNewBoard);
-        }
+         }
+      }
+      else
+      {
+         // Start default game
+         gameState = game.Init(gameSettings.BoardRows, gameSettings.BoardCols, gameSettings.DigitsOnNewBoard);
+      }
 
-        Log.Debug("Game starts");
+      while (true)
+      {
+         ConsoleIo.ScreenView(gameState, colorSettings);
 
-        while (true)
-        {
-            ConsoleIo.ScreenView(gameState, colorSettings);
+         Direction? direction = null;
+         var input = Console.ReadKey(true);
 
-            Direction? direction = null;
-            var input = Console.ReadKey(true);
-
-            if (input.Key == ConsoleKey.Q)
+         if (input.Key == ConsoleKey.Q)
+         {
+            // Save and exit game
+            File.WriteAllText(savedGameFile, JsonConvert.SerializeObject(gameState, Formatting.Indented));
+            Environment.Exit(0);
+         }
+         else if (input.Key == ConsoleKey.N)
+         {
+            // Start default game
+            gameState = game.Init(gameSettings.BoardRows, gameSettings.BoardCols, gameSettings.DigitsOnNewBoard);
+         }
+         else
+         {
+            direction = input.Key switch
             {
-                // Save and exit game
-                Log.Debug("Game over!");
-                File.WriteAllText(savedGameFile, JsonConvert.SerializeObject(gameState, Formatting.Indented));
-                Environment.Exit(0);
-            }
-            else if (input.Key == ConsoleKey.N)
-            {
-                // Start default game
-                gameState = game.Init(gameSettings.BoardRows, gameSettings.BoardCols, gameSettings.DigitsOnNewBoard);
-            }
-            else
-            {
-                direction = input.Key switch
-                {
-                    ConsoleKey.UpArrow => Direction.Up,
-                    ConsoleKey.DownArrow => Direction.Down,
-                    ConsoleKey.LeftArrow => Direction.Left,
-                    ConsoleKey.RightArrow => Direction.Right,
-                    _ => null
-                };
-            }
+               ConsoleKey.UpArrow => Direction.Up,
+               ConsoleKey.DownArrow => Direction.Down,
+               ConsoleKey.LeftArrow => Direction.Left,
+               ConsoleKey.RightArrow => Direction.Right,
+               _ => null
+            };
+         }
 
-            if (direction is not null)
-            {
-                gameState = game.NextState(gameState, direction, 1);
-            }
-        }
-    }
+         if (direction is not null)
+         {
+            gameState = game.NextState(gameState, direction, 1);
+         }
+      }
+   }
 }
--- Code/ConsoleApp/ConsoleIo.cs
+++ Code/ConsoleApp/ConsoleIo.cs
@@ -5,27 +5,27 @@ namespace ConsoleApp;
 
 public static class ConsoleIo
 {
-    public static void ScreenView(GameState output, ColorSettings colorSettings)
-    {
-        Console.Clear();
-        Console.WriteLine();
+   public static void ScreenView(GameState output, ColorSettings colorSettings)
+   {
+      Console.Clear();
+      Console.WriteLine();
 
-        for (var rows = 0; rows < output.Board.GetLength(0); rows++)
-        {
-            for (var cols = 0; cols < output.Board.GetLength(1); cols++)
-            {
-                Console.Write($"{output.Board[rows, cols],5}", Colors.FontColor(output.Board[rows, cols], colorSettings));
-            }
+      for (var rows = 0; rows < output.Board.GetLength(0); rows++)
+      {
+         for (var cols = 0; cols < output.Board.GetLength(1); cols++)
+         {
+            Console.Write($"{output.Board[rows, cols],5}", Colors.FontColor(output.Board[rows, cols], colorSettings));
+         }
 
-            Console.Write(Environment.NewLine + Environment.NewLine);
-        }
+         Console.Write(Environment.NewLine + Environment.NewLine);
+      }
 
-        if (output.IsGameOver)
-        {
-            Console.Write(" Game is over! ", colorSettings.ScoreColor);
-        }
+      if (output.IsGameOver)
+      {
+         Console.Write(" Game is over! ", colorSettings.ScoreColor);
+      }
 
-        Console.WriteLine($" Score: {output.Score}" + Environment.NewLine, colorSettings.ScoreColor);
-        Console.Write(" Use arrow keys to move the tiles. Press N for new game, Q for save and exit.", colorSettings.TextColor);
-    }
+      Console.WriteLine($" Score: {output.Score}" + Environment.NewLine, colorSettings.ScoreColor);
+      Console.Write(" Use arrow keys to move the tiles. Press N for new game, Q for save and exit.", colorSettings.TextColor);
+   }
 }
